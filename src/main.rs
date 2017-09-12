173a0f23644b7daac7c6fe28ed3d9d8736daa8d2
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2017-09-12 23:26:24 +0200

    Split project into a library and a binary

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,5 @@
-#![feature(plugin)]
-#![plugin(rocket_codegen)]
-
-extern crate rocket;
-
-#[get("/hello/<name>/<age>")]
-fn hello(name: String, age: u8) -> String {
-    format!("Hello, {} year old named {}!", age, name)
-}
+extern crate dynonym;
 
 fn main() {
-    rocket::ignite().mount("/", routes![hello]).launch();
+    dynonym::serve();
 }
