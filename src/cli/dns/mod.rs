2cc8d9f6e33f4773b03dcd18a7980849c250905d
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2017-09-16 03:15:46 +0200

    Specify CLI args for 'dynonym dns ...'

--- src/cli/dns/mod.rs
+++ src/cli/dns/mod.rs
@@ -1,3 +1,5 @@
+pub mod update;
+
 use errors::*;
 
 use clap::{App, ArgMatches, SubCommand};
@@ -5,6 +7,8 @@ use clap::{App, ArgMatches, SubCommand};
 pub fn setup<'a, 'b>() -> App<'a, 'b> {
     SubCommand::with_name("dns")
         .about("Manually manages dynamic DNS resource records")
+
+        .subcommand(update::setup())
 }
 
 pub fn call(_args: &ArgMatches) -> Result<()> {
---
+++ src/cli/dns/update.rs
@@ -0,0 +1,37 @@
+use errors::*;
+
+use clap::{App, Arg, ArgMatches, SubCommand};
+
+pub fn setup<'a, 'b>() -> App<'a, 'b> {
+    SubCommand::with_name("update")
+        .about("Manually updates a dynamic DNS resource record")
+
+        .arg(
+            Arg::with_name("NAME")
+                .help("Specifies the resource record name (aka domain name)")
+                .required(true)
+                .index(1)
+        )
+        .arg(
+            Arg::with_name("a")
+                .short("4")
+                .long("a")
+                .value_name("A")
+                .help("Specifies the A resource record (aka IPv4 address)")
+                .takes_value(true)
+                .required_unless("aaaa")
+        )
+        .arg(
+            Arg::with_name("aaaa")
+                .short("6")
+                .long("aaaa")
+                .value_name("AAAA")
+                .help("Specifies the AAAA resource record (aka IPv6 address)")
+                .takes_value(true)
+                .required_unless("a")
+        )
+}
+
+pub fn call(_args: &ArgMatches) -> Result<()> {
+    unimplemented!()
+}
