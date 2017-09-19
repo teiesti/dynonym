9506c80ace7eefda6b9b6d0ed4053dff182d08e3
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2017-09-19 17:56:38 +0200

    Rename soa() and make it a member function

--- src/dns/dns_update.rs
+++ src/dns/dns_update.rs
@@ -1,10 +1,10 @@
+use super::Domain;
 use errors::*;
 
 use std::net::IpAddr::{self, V4, V6};
 use trust_dns::client::{Client, SyncClient};
 use trust_dns::op::ResponseCode;
-use trust_dns::rr::Name as Domain;
-use trust_dns::rr::{DNSClass, Name, RData, Record, RecordType};
+use trust_dns::rr::{DNSClass, RData, Record, RecordType};
 
 pub struct Provider {
     client: SyncClient,
@@ -12,12 +12,39 @@ pub struct Provider {
 
 impl Provider {
     // TODO
+
+    #[inline]
+    fn find_zone(&self, domain: &Domain) -> Result<Domain> {
+        for domain in
+        (0..domain.num_labels() + 1)
+        .rev()
+        .map(|x| domain.trim_to(x as usize))
+        {
+            let response = self.client.query(&domain, DNSClass::IN, RecordType::SOA)?;
+            let record =
+            response
+            .answers().iter()
+            .chain(
+                response
+                .name_servers().iter()
+                .filter(|x| x.rr_type() == RecordType::SOA)
+            )
+            .next()
+            ;
+            if let Some(record) = record {
+                let soa = record.name().clone();
+                assert!(soa.zone_of(&domain));
+                return Ok(soa);
+            }
+        }
+        unreachable!()
+    }
 }
 
 impl super::Provider for Provider {
     fn update(&self, domain: Domain, ip: IpAddr) -> Result<()> {
         // Find the zone name to update, i.e. SOA name
-        let origin = soa(&self.client, &domain)
+        let zone = self.find_zone(&domain)
             .chain_err(|| "Could not find zone for given domain")?;
 
         // Assemble the record
@@ -36,37 +63,10 @@ impl super::Provider for Provider {
         }
 
         // Send the update request
-        let result = self.client.append(record, origin, false)
+        let result = self.client.append(record, zone, false)
             .chain_err(|| "Could not update given domain")?;
         assert_eq!(result.response_code(), ResponseCode::NoError);
 
         Ok(())
     }
-}
-
-#[inline]
-fn soa(client: &SyncClient, name: &Name) -> Result<Name> {
-    for name in
-        (0..name.num_labels() + 1)
-            .rev()
-            .map(|x| name.trim_to(x as usize))
-    {
-        let response = client.query(&name, DNSClass::IN, RecordType::SOA)?;
-        let record =
-            response
-                .answers().iter()
-                .chain(
-                    response
-                        .name_servers().iter()
-                        .filter(|x| x.rr_type() == RecordType::SOA)
-                )
-                .next()
-        ;
-        if let Some(record) = record {
-            let soa = record.name().clone();
-            assert!(soa.zone_of(&name));
-            return Ok(soa);
-        }
-    }
-    unreachable!()
 }
