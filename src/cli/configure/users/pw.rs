76165a4749e6e14b4912821a8f67c8d7e62ef620
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2017-10-20 23:19:07 +0200

    Implement 'dynonym configure users pw'

--- src/cli/configure/users/pw.rs
+++ src/cli/configure/users/pw.rs
@@ -1,3 +1,4 @@
+use config::Config;
 use errors::*;
 
 use clap::{App, Arg, ArgMatches, SubCommand};
@@ -14,6 +15,26 @@ pub fn setup<'a, 'b>() -> App<'a, 'b> {
         )
 }
 
-pub fn call(_args: &ArgMatches) -> Result<()> {
-    unimplemented!()
+pub fn call(args: &ArgMatches) -> Result<()> {
+    // Load the config
+    let config_file = args.value_of("config").unwrap();
+    let mut config = Config::load(config_file)?;
+
+    // Find the user
+    let user = args.value_of("USER").unwrap();
+    match config.user_mut(user) {
+        Some(user) => {
+            // Prompt for a password
+            let pw = ::rpassword::prompt_password_stdout("Please enter a password: ")?;
+
+            // Change the password
+            user.pw = pw.as_str().into();
+        },
+        None => bail!(format!("A user named '{}' does not exist", user)), // TODO Use proper error!
+    }
+
+    // Store the config
+    config.store(config_file)?;
+
+    Ok(())
 }
